542abc14dde393478c3e39c2f0a5639001426085
Min Kang <86979005+minkang3@users.noreply.github.com>
2024-11-01 13:39:21 -0700

    Loss function

---
+++ loss.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+from utils import intersection_over_union
+
+class YoloLoss(nn.Module):
+    def __init__(self, S=7, B=2, C=20):
+        super(YoloLoss, self).__init__()
+        self.mse = nn.MSELoss(reduction="sum")
+        self.S = S
+        self.B = B
+        self.C = C
+        self.lambda_noobj = 0.5
+        self.lambda_coord = 5
+
+    def forward(self, predictions, target):
+        predictions = predictions.reshape(-1, self.S, self.S, self.C + self.B*5)
+
+        iou_b1
